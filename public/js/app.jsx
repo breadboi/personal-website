24c436dc65fa53941aac40ccb6975ddf93690dad
breadboi <brett.carney1273@gmail.com>
2020-01-03 17:42:08 -0600

    Completed #1 by finishing all requirements

--- public/js/app.jsx
+++ public/js/app.jsx
@@ -1,13 +1,35 @@
-class HelloWorld extends React.Component {
-    render() {
-      return (
-        <div className="container">
-          <div className="row text-center">
-            <img src="assets/me.jpg" alt="Avatar" className="circular"/> 
+class HelloWorld extends React.Component
+{
+  render()
+  {
+    return (
+      <div className="container">
+        <div className="row text-center">
+          <img src="assets/me.png" alt="Avatar" className="circular" />
+        </div>
+
+        <div className="row text-center">
+          <div className="col">
+            <h1>Brett Carney</h1>
           </div>
         </div>
-      );
-    }
+
+        <div className="row text-center">
+          <div className="col">
+            <h2>Software Developer</h2>
+          </div>
+        </div>
+
+        <div className="row text-center">
+          <div className="col">
+            <a href="assets/resume-samples.pdf" class="btn btn-lg" title="View Resumé">
+              <i class="zmdi zmdi-file"></i> View CV
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
-  
-  ReactDOM.render(<HelloWorld />, document.getElementById("app"));
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("app"));
