ed6e920fab1b4253601ddb7a04b9d177ce75cf6a
Brett Carney <brett.carney1273@gmail.com>
2020-01-05 11:20:19 -0600

    #7 Set card size to work across more devices

--- public/js/projects.jsx
+++ public/js/projects.jsx
@@ -48,8 +48,8 @@ class Project extends React.Component {
 
     render() {
         return (
-            <div className="col-lg-2 col-md-4 col-sm-6 col-xs-12">
-                <div className="card project-card">
+            <div className="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div className="card project-card mobile-size-card">
                     <div className="card-body">
                         <h4 className="card-title">{this.props.project.FullName}</h4>
                         <p className="card-text">{this.props.project.Description}</p>                        
