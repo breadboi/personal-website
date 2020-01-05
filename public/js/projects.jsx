ad32db1598f7e22753642638a8022b448cb6a3ba
Brett Carney <brett.carney1273@gmail.com>
2020-01-05 10:38:51 -0600

    #7 Fixed horizontal scrolling and adjusted columns for project cards

--- public/js/projects.jsx
+++ public/js/projects.jsx
@@ -48,7 +48,7 @@ class Project extends React.Component {
 
     render() {
         return (
-            <div className="col">
+            <div className="col-lg-2 col-md-4 col-sm-6 col-xs-12">
                 <div className="card project-card">
                     <div className="card-body">
                         <h4 className="card-title">{this.props.project.FullName}</h4>
