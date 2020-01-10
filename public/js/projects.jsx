6dfba1f46603c45bf5110e3302bf420cb29a0100
breadboi <brett.carney1273@gmail.com>
2020-01-09 19:00:37 -0600

    Updated spacing and converted list to cards for skills

--- public/js/projects.jsx
+++ public/js/projects.jsx
@@ -26,7 +26,7 @@ class Projects extends React.Component {
                 <div className="grid grid-pad">
 
                     <div className="row text-center projects-header">
-                        <div className="col-12">
+                        <div className="col-12 section-header">
                             <h2>Projects</h2>
                             <p>These are the most recent projects I've made commits to.</p>
                         </div>
@@ -53,7 +53,7 @@ class Project extends React.Component {
     render() {
         return (
             <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <div className="card project-card">
+                <div className="card project-card shadow">
                     <div className="card-body">
                         <h4 className="card-title">{this.props.project.FullName}</h4>
                         <p className="card-text">{this.props.project.Description}</p>
--- public/js/skills.jsx
+++ public/js/skills.jsx
@@ -7,33 +7,60 @@ class Skills extends React.Component
                 <div className="gird grid-pad">
 
                     <div className="row text-center">
-                        <div className="col">
-                            <h3>Highlighted Skills</h3>
+                        <div className="col section-header">
+                            <h2>Highlighted Skills</h2>
                         </div>
                     </div>
 
                     <div className="row skills-row">
-                        <ul className="col zmdi-hc-ul">
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>C#</li>
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Golang</li>
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>PHP</li>
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Java</li>
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Python</li>
-                        </ul>
 
-                        <ul className="col zmdi-hc-ul">
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>React</li>
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Jquery</li>
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>ASP.NET</li>
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Gin</li>
-                        </ul>
+                        <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                            <div className="card project-card shadow">
+                                <div className="card-body">
+                                    <h4 className="card-title">Back End</h4>
+                                    <ul className="col card-text zmdi-hc-ul">
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>C#</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Golang</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>PHP</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Java</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>C++</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Python</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
 
-                        <ul className="col zmdi-hc-ul">
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Linux</li>
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Windows</li>
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Visual Studio/Code</li>
-                            <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Agile Development</li>
-                        </ul>
+                        <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                            <div className="card project-card shadow">
+                                <div className="card-body">
+                                    <h4 className="card-title">Front End</h4>
+                                    <ul className="col zmdi-hc-ul">
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>React</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Jquery</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>ASP.NET</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Gin</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>AJAX</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Bootstrap</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                            <div className="card project-card shadow">
+                                <div className="card-body">
+                                    <h4 className="card-title">Other Technologies and Skills</h4>
+                                    <ul className="col zmdi-hc-ul">
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Git</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Github and TFVC</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Agile Development</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Visual Studio/Code</li>
+                                        <li><i class="zmdi-hc-li zmdi zmdi-dot-circle-alt"></i>Linux Varients</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
 
                 </div>
