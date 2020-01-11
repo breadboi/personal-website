3c0d9b2b4b9988247318ebf371ea7b4f75d477e6
breadboi <brett.carney1273@gmail.com>
2020-01-10 20:09:01 -0600

    #7 Layout for desktop and mobile

--- public/js/links.jsx
+++ public/js/links.jsx
@@ -7,11 +7,11 @@ class Links extends React.Component
                 <div className="gird grid-pad">
 
                     <div class="link-container text-center row">
-                        <a href="https://github.com/breadboi" target="_blank" class="col"><i class="link-section-icon zmdi zmdi-github-box"></i></a>
+                        <a href="https://github.com/breadboi" target="_blank" class="col links-section-a" onDragStart={(e) => { e.preventDefault() }}><i class="link-section-icon shadow-lg zmdi zmdi-github-box" onDragStart={(e) => { e.preventDefault() }}></i></a>
 
-                        <a href="https://github.com/breadboi" target="_blank" class="col"><i class="link-section-icon zmdi zmdi-email"></i></a>
+                        <a href="https://github.com/breadboi" target="_blank" class="col links-section-a" onDragStart={(e) => { e.preventDefault() }}><i class="link-section-icon shadow-lg zmdi zmdi-email"></i></a>
 
-                        <a href="https://github.com/breadboi" target="_blank" class="col"><i class="link-section-icon zmdi zmdi-linkedin-box"></i></a>
+                        <a href="https://github.com/breadboi" target="_blank" class="col links-section-a" onDragStart={(e) => { e.preventDefault() }}><i class="link-section-icon shadow-lg zmdi zmdi-linkedin-box"></i></a>
                     </div>
 
                 </div>
