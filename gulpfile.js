88e17680c7c8cc8677e5108b7a0ef3ca6fd2ada0
Brett Carney <brett.carney1273@gmail.com>
2020-07-20 00:08:57 -0500

    #14 Added additional gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,61 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+const minify = require('gulp-minify');
 
-gulp.task('babel', function(){
+/**
+ * Convert jsx to js
+ */
+gulp.task('babel', function() {
     return gulp.src("src/jsx/*.jsx")
         .pipe(babel())
-        .pipe(gulp.dest("public/js/"));
+        .pipe(gulp.dest("src/react-js/"));
 });
 
-gulp.task('js', function() {
+/**
+ * Move vendor js scripts
+ */
+gulp.task('vendor', function() {
+    return gulp.src("src/vendor/*.js")
+        .pipe(gulp.dest("public/vendor/"));
+});
+
+/**
+ * Minify react js files
+ */
+gulp.task('minify-react', function() {
+    return gulp.src("src/react-js/*.js")
+        .pipe(minify({noSource: true}))
+        .pipe(gulp.dest("public/js/"))
+});
+
+/**
+ * Minify js files
+ */
+gulp.task('minify-js', function() {
+    return gulp.src("src/js/*.js")
+        .pipe(minify({noSource: true}))
+        .pipe(gulp.dest("public/js/"))
+});
+
+/**
+ * Minify all js and react-js
+ */
+gulp.task("minify-all", gulp.series('minify-react', 'minify-js'));
+
+/**
+ * Copy regular js to public
+ */
+gulp.task('copy-js', function() {
     return gulp.src("src/js/*.js")
         .pipe(gulp.dest("public/js/"));
 });
 
-gulp.task('vendor', function() {
-    return gulp.src("src/vendor/*.js")
-        .pipe(gulp.dest("public/vendor"));
-});
+/**
+ * Build with all src unminified
+ */
+gulp.task('build-debug', gulp.series('babel', 'copy-js', 'vendor'));
+
+/**
+ * Build with all src minified
+ */
+gulp.task('build-release', gulp.series('babel', 'minify-all', 'vendor'));
