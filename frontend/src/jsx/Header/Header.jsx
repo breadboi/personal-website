48605084ba987037e48d406448154a105fcf37ec
Brett Carney <brett@brettcarney.com>
2022-08-15 10:40:47 -0500

    Updated pfp to use github and also added a more recent resume (#47)

--- frontend/src/jsx/Header/Header.jsx
+++ frontend/src/jsx/Header/Header.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import { Button } from "@mui/material";
-import myAvatar from '../../../assets/ProfPhoto300x300.png';
+import myResume from '../../../assets/Resume_BrettCarney_2021.pdf'
 
 export function Header({ }) {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
       <Avatar
         alt="Brett Carney"
-        src={myAvatar}
+        src='https://avatars.githubusercontent.com/u/32111055?v=4'
         sx={{ width: 300, height: 300 }}
       />
       <h1>Brett Carney</h1>
       <h2>Software Engineer</h2>
-      <Button variant="outlined" href='../../../assets/Resume_BrettCarney.PDF'>
+      <Button variant="outlined" href={myResume}>
         Resume
       </Button>
     </Box>
