82da4d473699ed14311c13060c50167cabfeb6dc
breadboi <brett@brettcarney.com>
2022-10-07 19:32:52 -0500

    Projects card button changes and styling changes

--- frontend/src/jsx/BreakdownBar/BreakdownBar.jsx
+++ frontend/src/jsx/BreakdownBar/BreakdownBar.jsx
@@ -12,7 +12,7 @@ export function BreakdownBar(props) {
                                     style={{
                                         backgroundColor: props.colorMap[language],
                                         width: Math.max(
-                                            (props.breakdownItems[language]) * props.width,
+                                            (props.breakdownItems[language] / 100) * props.width,
                                             5
                                         ),
                                         height: 10,
@@ -35,40 +35,43 @@ export function BreakdownBar(props) {
             <div style={{ width: props.width }}>
                 <ul style={{ display: "flex", flexWrap: "wrap", listStyleType: "none", margin: 0, padding: 0, overflow: 'hidden' }}>
                     {Object.keys(props.breakdownItems).map((language) => {
-                        return (
-                            <li
-                                key={`${language}-name`}
-                                style={{
-                                    margin: 2,
-                                    display: "flex",
-                                    justifyItems: "center",
-                                    alignItems: "center"
-                                }}
-                            >
-                                <span
+                        // Remove if value is < 25%
+                        if (parseInt(props.breakdownItems[language]) >= 20) {
+                            return (
+                                <li
+                                    key={`${language}-name`}
                                     style={{
-                                        height: 10,
-                                        width: 10,
-                                        backgroundColor: props.colorMap[language],
-                                        borderRadius: "50%",
-                                        display: "inline-block"
-                                    }}
-                                />
-                                <span
-                                    style={{
-                                        fontWeight: 700,
-                                        marginLeft: 5,
-                                        marginRight: 5,
-                                        color: props.textColor || "black"
+                                        margin: 2,
+                                        display: "flex",
+                                        justifyItems: "center",
+                                        alignItems: "center"
                                     }}
                                 >
-                                    {language}
-                                </span>
-                                <span style={{ color: props.lightColor || "gray" }}>
-                                    {(props.breakdownItems[language]).toFixed(1)}%
-                                </span>
-                            </li>
-                        );
+                                    <span
+                                        style={{
+                                            height: 10,
+                                            width: 10,
+                                            backgroundColor: props.colorMap[language],
+                                            borderRadius: "50%",
+                                            display: "inline-block"
+                                        }}
+                                    />
+                                    <span
+                                        style={{
+                                            fontWeight: 700,
+                                            marginLeft: 5,
+                                            marginRight: 5,
+                                            color: props.textColor || "black"
+                                        }}
+                                    >
+                                        {language}
+                                     </span>
+                                    <span style={{ color: props.lightColor || "gray" }}>
+                                        {(props.breakdownItems[language]).toFixed(1)}%
+                                    </span>
+                                </li>
+                            );
+                        }
                     })}
                 </ul>
             </div>
--- frontend/src/jsx/Projects/Projects.jsx
+++ frontend/src/jsx/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import React, { useEffect, useState } from "react";
 import { BreakdownBar } from "../BreakdownBar/BreakdownBar";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const styles = {
     card: {
@@ -18,6 +19,9 @@ const styles = {
         // Justify the content so that CardContent will always be at the top of the card,
         // and CardActions will be at the bottom
         justifyContent: "space-between"
+    },
+    githubButton: {
+        backgroundColor: "black"
     }
 }
 
@@ -48,19 +52,20 @@ const Project = props => {
     return (
         <Card style={styles.card} sx={{ minWidth: 275, maxWidth: 275, m: 2, height: 300}} >
             <CardContent>
-                <Typography component={'span'} sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    <h4 className="card-title">{props.project.Name}</h4>
-                    <p className="card-text">{props.project.Description}</p>
-                </Typography>
+
+                <h3 className="card-title">{props.project.Name}</h3>                  
+                <p className="card-text">{props.project.Description}</p>
 
                 <BreakdownBar breakdownItems={props.project.Languages} colorMap={props.languageColors} width={200}></BreakdownBar>
 
             </CardContent>
+
             <CardActions>
-                <Button stylevariant="outlined" href={"https://github.com/" + props.project.FullName} target="_blank">
+                <Button style={styles.githubButton} variant="contained" startIcon={<GitHubIcon />}>
                     See Source
                 </Button>
             </CardActions>
+
         </Card>
     );
 };
