76521c18d7bd0592808e10b9a6234531e6938355
Brett Carney <brett@brettcarney.com>
2023-06-04 23:12:01 -0500

    Updating alt text

--- components/MyNavbar.tsx
+++ components/MyNavbar.tsx
@@ -16,7 +16,7 @@ export default function MyNavbar({ }) {
                 <img
                     src="LogoIcon.png"
                     className="mr-3 h-6 sm:h-9"
-                    alt="Brett Carney Logo"
+                    alt="Personal Logo"
                 />
                 <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
                     BC
